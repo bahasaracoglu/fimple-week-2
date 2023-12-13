c6fdefb13447d9e305fd4acd2f9646f4b944994e
bahasaracoglu <bahasaracoglu@gmail.com>
2023-12-13 07:52:23 +0300

    searchInput added.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 console.log("hey");
 
+let data = [];
+
 const fetchData = async () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
   const res = await fetch(url);
-  const data = await res.json();
+  data = await res.json();
   console.log(data);
   return data;
 };
 
 const listData = (data) => {
+  console.log("lisdata");
   const listContainer = document.querySelector(".list");
+  listContainer.innerHTML = "";
 
   data.forEach((element) => {
     let liElement = document.createElement("li");
-    liElement.innerHTML = element.title;
+    liElement.innerHTML = `<div>
+                           <h2>${element.title}</h2><button>X</button>
+                           </div>
+                           <p>${element.body}</p>`;
     listContainer.appendChild(liElement);
   });
   console.log(listContainer);
 };
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("input", (event) => {
+  let query = event.target.value;
+  console.log(query);
+  let result = data.filter(
+    (post) => post.title.includes(query) || post.body.includes(query)
+  );
+  listData(result);
+  console.log(result);
+});
 
 const init = async () => {
   const data = await fetchData();
