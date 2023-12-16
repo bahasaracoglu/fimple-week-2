7e8d44580f6fc923d00a54b4728b1b47e8bb788b
bahasaracoglu <bahasaracoglu@gmail.com>
2023-12-16 09:15:22 +0300

    comments modal

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-console.log("hey");
-
-let data = [];
+let data;
+console.log("dataa", data);
 
 const fetchData = async () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
   const res = await fetch(url);
   data = await res.json();
-  console.log(data);
   return data;
 };
 
+const getPostDetail = async (post) => {
+  const postId = post.id;
+  const url = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+  const res = await fetch(url);
+  const commentData = await res.json();
+  console.log("postModal", commentData);
+  return commentData;
+};
+
+const showComments = (comments) => {
+  const modal = document.querySelector(".modal");
+  console.log(modal);
+  modal.style.display = "block";
+  const commentList = document.querySelector(".comment-list");
+  commentList.innerHTML = "";
+
+  comments.forEach((comment) => {
+    const commentName = comment.name;
+    const commentBody = comment.body;
+    const liElement = document.createElement("li");
+    liElement.innerHTML = `<div><h2>
+                            ${commentName}</h2>
+                            <p> ${commentBody}</p>
+                            </div>`;
+
+    commentList.appendChild(liElement);
+  });
+};
+
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 const listData = (data) => {
-  console.log("lisdata");
+  console.log("listdata");
   const listContainer = document.querySelector(".list");
   listContainer.innerHTML = "";
 
@@ -23,7 +62,7 @@ const listData = (data) => {
                           <span class=cardTitleCont> 
                            <h2>${
                              element.title
-                           }</h2><span class="deleteBtnCont"><button class="postDeleteBtn">&#10006;</button></span></span>
+                           }</h2><span class="deleteBtnCont"><button class="postDeleteBtn"><i class="material-icons">close</i></button></span></span>
                            <p>${
                              postBody.charAt(0).toUpperCase() +
                              postBody.slice(1) +
@@ -33,7 +72,14 @@ const listData = (data) => {
     listContainer.appendChild(liElement);
 
     let deleteBtn = liElement.querySelector(".postDeleteBtn");
-    deleteBtn.addEventListener("click", () => deletePost(liElement));
+    deleteBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      deletePost(liElement);
+    });
+    liElement.addEventListener("click", async (e) => {
+      const comments = await getPostDetail(element);
+      showComments(comments);
+    });
   });
   console.log(listContainer);
 };
@@ -42,33 +88,36 @@ const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchButton");
 const searchClearBtn = document.getElementById("searchClear");
 
-searchInput.addEventListener("mouseover", () => {
-  searchBtn.mouseover();
-
-  searchClearBtn.mouseover();
-});
+let timer;
+const filterData = (query) => {
+  console.log("filter", data);
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    let result = data.filter(
+      (post) => post.title.includes(query) || post.body.includes(query)
+    );
+    console.log("result", result);
+    if (result.length > 0) {
+      listData(result);
+    } else {
+      const listContainer = document.querySelector(".list");
+      listContainer.innerHTML =
+        "<div class='notFound'><img src='./john-travolta.gif'/><h2>Result Not Found :(</h2><p>Whoops... we couldn't find what you're looking for</p></div>";
+    }
+    console.log(result);
+  }, 600);
+};
 
 searchInput.addEventListener("input", (event) => {
   let query = event.target.value.toLowerCase();
   console.log(query);
-  let result = data.filter(
-    (post) => post.title.includes(query) || post.body.includes(query)
-  );
-  console.log("result", result);
-  if (result.length > 0) {
-    listData(result);
-  } else {
-    const listContainer = document.querySelector(".list");
-    listContainer.innerHTML =
-      "<div class='notFound'><img src='./john-travolta.gif'/><h2>Result Not Found :(</h2><p>Whoops... we couldn't find what you're looking for</p></div>";
-  }
-
-  console.log(result);
+  filterData(query);
 });
 
 searchClearBtn.addEventListener("click", () => {
   searchInput.value = "";
   searchInput.focus();
+  filterData("");
 });
 
 const deletePost = (element) => {
@@ -81,11 +130,9 @@ const deletePost = (element) => {
 };
 
 const init = async () => {
-  const data = await fetchData();
+  data = await fetchData();
   console.log(data);
   listData(data);
 };
-
-document.addEventListener("DOMContentLoaded", () => {});
 
 init();
